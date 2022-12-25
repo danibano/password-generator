607b3e6f0f1bdd94ac40c8cc2442dbe1c53be3c0
danibano <banovetz.danielle@gmail.com>
2022-12-25 15:07:02 -0600

    added prompts for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,27 @@
 // Assignment Code
+
+// [] GIVEN I need a new, secure password
+// [] WHEN I click the button to generate a password
+// [] THEN I am presented with a series of prompts for password criteria
+// [] WHEN prompted for password criteria
+// [] THEN I select which criteria to include in the password
+// [] WHEN prompted for the length of the password
+// [] THEN I choose a length of at least 8 characters and no more than 128 characters
+// [] WHEN asked for character types to include in the password
+// [] THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// [] WHEN I answer each prompt
+// [] THEN my input should be validated and at least one character type should be selected
+// [] WHEN all prompts are answered
+// [] THEN a password is generated that matches the selected criteria
+// [] WHEN the password is generated
+// [] THEN the password is either displayed in an alert or written to the page
+
 var generateBtn = document.querySelector("#generate");
 var numberOfCharacters;
 var okToUseSpecialChars;
+var okToUseLowLets;
+var okToUseUpLets;
+var okToUseNumbs;
 var finalPassword = "";
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "_", "=", "`", "~", "[", "]", "{", "}", ";", ":", ",", ".", "<", ">", "/", "?"];
 var lowerLetters = ("abcdefghijklmnopqrstuvwxyz").split("");
@@ -17,9 +37,16 @@ var numbers = ("0123456789").split("");
 function askQuestions (){
   
   var response = prompt ("How many characters");
-  var numberOfCharacters = parseInt(response);
+  numberOfCharacters = parseInt(response);
 
-  var okToUseSpecialChars = confirm("Do you want to use special characters?");
+  okToUseSpecialChars = confirm("Do you want to use special characters?");
+
+  okToUseLowLets = confirm("Do you want to use lower case letters?");
+
+  okToUseUpLets = confirm("Do you want to use upper case letters?");
+
+  okToUseNumbs = confirm("Do you want to use numbers?");
+
 }
 
 // use a loop to grab a random charcter x nunber of times
@@ -31,7 +58,6 @@ function askQuestions (){
 //homework starts here
 function generatePassword(){
   askQuestions();
-  //var numberOfCharacters = parseInt( prompt("How many Characters?"))
 
 }
 
