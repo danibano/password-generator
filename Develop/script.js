76d56e68e6f59f199e64d4cb1cacdf22873af995
danibano <banovetz.danielle@gmail.com>
2022-12-25 14:55:54 -0600

    added global var

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,43 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numberOfCharacters;
+var okToUseSpecialChars;
+var finalPassword = "";
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "_", "=", "`", "~", "[", "]", "{", "}", ";", ":", ",", ".", "<", ">", "/", "?"];
+var lowerLetters = ("abcdefghijklmnopqrstuvwxyz").split("");
+var upperLetters = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
+var numbers = ("0123456789").split("");
+
+// use global variables to list out alll possible: mayber use an array
+//special characters
+//lower case letters
+//upper case letters
+//numbers
+
+function askQuestions (){
+  
+  var response = prompt ("How many characters");
+  var numberOfCharacters = parseInt(response);
+
+  var okToUseSpecialChars = confirm("Do you want to use special characters?");
+}
+
+// use a loop to grab a random charcter x nunber of times
+
+// finalPassword += 
+
+//most can be done with arrays loops and functions apparently
+
+//homework starts here
+function generatePassword(){
+  askQuestions();
+  //var numberOfCharacters = parseInt( prompt("How many Characters?"))
+
+}
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
